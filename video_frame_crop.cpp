46a6517cbde9105d38cd31af5c96cb5ebecf1fd5
llpspark <llpspark@outlook.com>
2017-11-18 21:08:12 +0800

    update video_frame.cpp make it can put text to the video

--- video_frame_crop.cpp
+++ video_frame_crop.cpp
@@ -9,13 +9,14 @@ using namespace cv;
 int main()
 {
 	//input and output
-	string video_path = "2.MP4", output_video = "out.avi";
-	int crop_x = 1000; int crop_y = 700; int crop_w = 1800; int crop_h = 1000;
+	string video_path = "1.rmvb", output_video = "out.avi";
+	string text_str = "COPYRIGHT @ LIPENG LIU";
+	int crop_x = 10; int crop_y = 15; int crop_w = 160; int crop_h = 40;
 	
 	//define read and write video
 	VideoCapture cap; VideoWriter writer;
 	cap.open(video_path);
-	writer.open(output_video, CV_FOURCC('M', 'J', 'P', 'G'), cap.get(CV_CAP_PROP_FPS), Size(static_cast<int>(cap.get(CV_CAP_PROP_FRAME_WIDTH)), static_cast<int>(cap.get(CV_CAP_PROP_FRAME_HEIGHT))), true);
+	writer.open(output_video, CV_FOURCC('M', 'J', 'P', 'G'), cap.get(CV_CAP_PROP_FPS), Size(static_cast<int>(cap.get(CV_CAP_PROP_FRAME_WIDTH)), static_cast<int>(cap.get(CV_CAP_PROP_FRAME_HEIGHT))), true);//Size(crop_w, crop_h)
 	
 	//print some information about video
 	cout << cap.get(CV_CAP_PROP_FRAME_WIDTH) << endl;
@@ -34,6 +35,7 @@ int main()
 		//plot rectangle with read line
 		rectangle(frame, point1, point2, Scalar(0, 0, 255));
 		namedWindow(win_name, WINDOW_AUTOSIZE);
+		putText(frame, text_str, Point(crop_x, crop_y + crop_h - 2), FONT_HERSHEY_SIMPLEX, 0.4 ,Scalar(0, 0, 255));
 		writer << frame;
  		imshow(win_name, frame);
 		if(waitKey(1) == 27)
