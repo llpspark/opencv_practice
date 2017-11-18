54b4d82dab4ff4133005b55eeded4eec631ed3c9
llpspark <llpspark@outlook.com>
2017-11-18 16:08:57 +0800

    update plot_coordinate.cpp make the implementation without file's input/output

--- plot_coordinate.cpp
+++ plot_coordinate.cpp
@@ -7,40 +7,35 @@
 using namespace std;
 using namespace cv;
 
-
+//draw rectangle function
 void draw_rect(Mat& src, Point p1, Point p2)
 {
 	rectangle(src, p1, p2, Scalar(0, 0, 255));
 }
 
-vector<string> read_substr(string str, string pattern)
-{
-	vector<string> sub;
-	string :: size_type pos; unsigned int len;
-	while(str.find(pattern, 0) != string::npos)
-	{
-		len = str.size();
-		pos = str.find(pattern, 0);
-		sub.push_back(str.substr(0, pos));
-		str = str.substr(pos + 1, len - pos -1);
-	}
-	sub.push_back(str);
-	return sub;
+bool click_flag = false;
+Point click_left;
 
-}
-
+//define mouse event  function
 void on_Mouse(int event, int x1, int y1, int flag, void* para)
 {
-	fstream file;
+	Point p1, p2;
 	Mat& img = *static_cast<Mat *>(para);
 	if(event == EVENT_LBUTTONDOWN)
 		{
+			click_flag = true;
 			cout << "(" << x1 <<","<< y1 << ")" << endl;
-			//draw_point(img, x, y);
-			file.open("a.txt", ios::app|ios::out);
-			file << x1 << " " << y1 << endl;
-			file.close();
+			click_left = Point(x1, y1);
 		}
+	else if(click_flag && event == EVENT_RBUTTONDOWN)
+	{
+		cout << "(" << x1 <<","<< y1 << ")" << endl;
+		p1.x = click_left.x;
+		p1.y = click_left.y;
+		p2 = Point(x1, y1);
+		draw_rect(img, p1, p2);
+		click_flag = false;
+	}
 }
 
 int main()
@@ -58,27 +53,7 @@ int main()
 			destroyWindow(winname);
 			break;
 		}
-		
 	}
-	fstream file;
-	file.open("a.txt", ios:: in);
-	Point p1, p2;
-
-	string str; vector<string> sub;
-	getline(file, str);
-	sub = read_substr(str, " ");
-	p1.x = atoi(sub[0].c_str());
-	p1.y = atoi(sub[1].c_str());
-	getline(file, str);
-	sub = read_substr(str, " ");
-	p2.x = atoi(sub[0].c_str());
-	p2.y = atoi(sub[1].c_str());
-
-	file.close();
-	draw_rect(img, p1, p2);
-	imshow(winname, img);
-	waitKey(2222);
 	imwrite("plot.jpg", img);
-
 	return 0;
 }
